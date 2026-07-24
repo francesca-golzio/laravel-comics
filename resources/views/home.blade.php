1ea064e4653f1291fe226871064530c0eb7a146e
francesca-golzio <francescagolzio@gmail.com>
2026-07-24 18:29:39 +0200

    feat(layout): add main layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Laravel Comics</title>
-</head>
-<body>
-  <h1>Laravel Comics</h1>
-  
-</body>
-</html>
+@extends('layouts.main')
+
+@section('contenuto')
+-- contenuto della pagina qui --
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Laravel Comics</title>
+</head>
+<body>
+  <header> -- qui header --</header>
+  
+  @yield('contenuto')
+  
+  <footer> -- qui footer --</footer>
+</body>
+</html>
