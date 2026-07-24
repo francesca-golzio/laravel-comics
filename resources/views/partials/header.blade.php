1d23ebe2464c4e690620a51f0a5a929cfbc7fe61
francesca-golzio <francescagolzio@gmail.com>
2026-07-24 20:05:06 +0200

    feat(ui): set header css properties

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,50 +1,50 @@
 <header>
 
-  <div class="container">    
+  <div class="container d-flex justify-content-between py-2">    
     
-    <img id="logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
+    <img class="align-self-top" id="logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg align-self-bottom p-0">
       <div class="container-fluid">        
           <ul class="navbar-nav">
 
-            <li class="nav-item">
+            <li class="nav-item text-uppercase">
               <a class="nav-link" href="#">caracters</a>
             </li>
 
-            <li class="nav-item">
-              <a class="nav-link" href="#">comics</a>
+            <li class="nav-item text-uppercase">
+              <a class="nav-link current" href="#">comics</a>
             </li>
 
-            <li class="nav-item">
+            <li class="nav-item text-uppercase">
               <a class="nav-link" href="#">movies</a>
             </li>
 
-            <li class="nav-item">
+            <li class="nav-item text-uppercase">
               <a class="nav-link" href="#">tv</a>
             </li>
 
-            <li class="nav-item">
-              <a class="nav-link" href="#">tv</a>
+            <li class="nav-item text-uppercase">
+              <a class="nav-link" href="#">games</a>
             </li>
 
-            <li class="nav-item">
+            <li class="nav-item text-uppercase">
               <a class="nav-link" href="#">collectibles</a>
             </li>
 
-            <li class="nav-item">
+            <li class="nav-item text-uppercase">
               <a class="nav-link" href="#">videos</a>
             </li>
 
-            <li class="nav-item">
+            <li class="nav-item text-uppercase">
               <a class="nav-link" href="#">fans</a>
             </li>
 
-            <li class="nav-item">
+            <li class="nav-item text-uppercase">
               <a class="nav-link" href="#">news</a>
             </li>
 
-            <li class="nav-item">
+            <li class="nav-item text-uppercase">
               <a class="nav-link" href="#">shop</a>
             </li>
 
