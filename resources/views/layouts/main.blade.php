0fc2b55f687799b63b879859337cbf92d1638526
francesca-golzio <francescagolzio@gmail.com>
2026-07-24 19:24:52 +0200

    feat(layout): add header (unstyled)

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,10 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+
   <title>Laravel Comics</title>
 </head>
 <body>
-  <header> -- qui header --</header>
+  @include('partials.header')
   
   @yield('contenuto')
   
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,59 @@
+<header>
+
+  <div class="container">    
+    
+    <img id="logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">        
+          <ul class="navbar-nav">
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">caracters</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">comics</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">movies</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">tv</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">tv</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">collectibles</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">videos</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">fans</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">news</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">shop</a>
+            </li>
+
+          </ul>
+      </div>
+    </nav>
+
+  </div>
+
+  <div class="hero"></div>
+
+</header>
