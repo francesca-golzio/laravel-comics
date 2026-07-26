2a116b8df84d07bd0460f808ad76c1c0233d1c47
francesca-golzio <francescagolzio@gmail.com>
2026-07-26 17:34:58 +0200

    feat(cards): render comics cards into ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.main')
 
-@section('contenuto')
--- contenuto della pagina qui --
+@php
+$comics = require('../././config/comics.php');
+@endphp
 
+@section('contenuto')
+  <div class="container">
+
+    <div class="half_up_label text-uppercase">popular comics</div>
+
+    <div class="row row-cols-6">
+
+      @foreach($comics as $comic)
+
+
+      <x-card>
+        <x-slot:imageUrl>{{ $comic['thumb'] }}</x-slot>        
+        <x-slot:title>{{ $comic['title'] }}</x-slot>
+      </x-card>
+
+      @endforeach
+
+    </div>
+  </div>
 @endsection
