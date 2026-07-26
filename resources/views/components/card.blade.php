abcf8ebd9b98ce23b36a5bad0d4b77f2cee72827
francesca-golzio <francescagolzio@gmail.com>
2026-07-26 17:33:10 +0200

    feat(card): add card component
    - add card component
    - add slot placeholders for cover images url and comics title
    - css style card

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,10 @@
+<div>
+
+    <div class="col py-3 mx-2">
+        <div class="card_img_container">
+            <img src="{{ $imageUrl }}" alt="comics cover" class="card_img">
+        </div>
+         <p class="card-text text-uppercase py-4">{{ $title }}</p>
+    </div>
+
+</div>
