97dc682694a20a7b1a79ec2601681880d11a7d43
francesca-golzio <francescagolzio@gmail.com>
2026-07-27 11:50:19 +0200

    fix : refactor detail-card
    now when called the detail-card requires only a :index parameter, all other data are dynamically provided by the component

--- resources/views/components/detail-card.blade.php
+++ resources/views/components/detail-card.blade.php
@@ -1,24 +1,27 @@
-
-
 <div class="container">
+    @php
+    $comics = require('../././config/comics.php');
+    $comic = $comics[$index];
+    @endphp
     
-    <div class="half_up_label text-uppercase">{{ $title }}</div>
+    <div class="half_up_label text-uppercase">{{ $comic['title'] }}</div>
     
     <div class="row g-5 p-4 mb-3 border border-primary">
   
-        <div class="col-6 fs-5">{{ $description }}</div>
+        <div class="col-6 fs-5">{{ $comic['description'] }}</div>
         
         <div class="col-3">
-            <img src="{{ $thumb }}" alt="cover" class="detail_cover">
+            <img src="{{ $comic['thumb'] }}" alt="cover" class="detail_cover">
         </div>
         
         <div class="col-3 info">
-            <div class="p-3">Series: {{ $series }}</div>
-            <div class="p-3">Main Artist: {{ $artists }}</div>
-            <div class="p-3">Main Writer: {{ $writers }}</div>
-            <div class="p-3">{{ $price }}</div>
+            <div class="p-3">Series: {{ $comic['series'] }}</div>
+            <div class="p-3">Main Artist: {{ $comic['artists'][0] }}</div>
+            <div class="p-3">Main Writer: {{ $comic['writers'][0] }}</div>
+            <div class="p-3">{{ $comic['price'] }}</div>
         </div>
 
     </div>    
     <br>
 </div>
+--------------------------
--- resources/views/detail-0.blade.php
+++ resources/views/detail-0.blade.php
@@ -1,19 +1,6 @@
 
 @extends('layouts.main')
 
-@php
-$comics = require('../././config/comics.php');
-@endphp
-
 @section('contenuto')
-
-<x-detail-card>
-  <x-slot:title>{{ $comics[0]['title'] }}</x-slot>
-  <x-slot:description>{{ $comics[0]['description'] }}</x-slot>
-  <x-slot:thumb>{{ $comics[0]['thumb'] }}</x-slot>
-  <x-slot:series>{{ $comics[0]['series'] }}</x-slot>
-  <x-slot:artists>{{ $comics[0]['artists'][0] }}</x-slot>
-  <x-slot:writers>{{ $comics[0]['writers'][0] }}</x-slot>
-  <x-slot:price>{{ $comics[0]['price'] }}</x-slot>
-</x-detail-card>
+<x-detail-card :index="0" />
 @endsection
